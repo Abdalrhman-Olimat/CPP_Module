75872c12c3e5fe4f10cf3f43cbc83cd05acf5cc8
Abdalrhman_olimat <abdalrhmanolimat911@gmail.com>
2025-08-25 12:56:58 +0300

    remove extra

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
-using namespace std;
+#include "main.hpp"
 
 int main() {
-    
+    PhoneBook pb;
+    std::string command;
+
+    std::cout << "📞 Welcome to My Awesome PhoneBook!\n";
+    std::cout << "Commands: ADD | SEARCH | EXIT\n";
+
+    while (true) {
+        std::cout << "\n> ";
+        if (!std::getline(std::cin, command))
+            break;
+        if (command == "ADD") {
+            pb.addContact();
+        } else if (command == "SEARCH") {
+            pb.searchContacts();
+        } else if (command == "EXIT") {
+            std::cout << "👋 Goodbye!\n";
+            break;
+        } else if (!command.empty()) {
+            std::cout << "❌ Unknown command. Try again.\n";
+        }
+    }
+    return 0;
 }
+
